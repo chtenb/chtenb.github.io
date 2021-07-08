a335128558bc97547869b8f24f5f1cd0d4802cf3
Chiel ten Brinke <ctenbrinke@gmail.com>
2021-07-08 23:03:38 +0200

    publish

--- docs/index.b75b8864.js
+++
@@ -1,2 +0,0 @@
-!function(){var n,o={};n=function(n){return function(){t(n)}};var t=function(n){var o=n+window.location.search+window.location.hash;window.location=o},a={setLocationBaseString:n}.setLocationBaseString("https://tabviewer.app");o={main:a},console.log("Starting app"),o.main()}();
-//# sourceMappingURL=index.b75b8864.js.map
